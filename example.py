8ea3e67981d15255eb4b15feca33e2c85a788cf1
Zhigang <wangzhigang1999@live.cn>
2023-10-24 09:50:57 +0800

    add example

--- example.py
+++ example.py
@@ -8,14 +8,14 @@ from sdk.base import AlgoTemplate
 class Example(AlgoTemplate):
 
     def build_model(self, hyper_params: dict) -> object:
-        # knn
+        # get hyper params to build your model
         hello = hyper_params.get('hello')
         assert hello == 'world', 'hello world'
         model = KNeighborsClassifier()
         return model
 
     def train(self, model: KNeighborsClassifier, df: pd.DataFrame, hyper_params: dict) -> (object, dict):
-        labels = np.random.randint(0, 2, size=(len(df), 1))
+        labels = np.random.randint(0, 1, size=(len(df), 1))
         model = model.fit(df, labels)
         return model, hyper_params
 
@@ -25,4 +25,5 @@ class Example(AlgoTemplate):
 
 
 if __name__ == '__main__':
+    # init  your model here
     Example()
